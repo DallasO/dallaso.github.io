5cb1fee299de459319bda961d8676c321e2ae656
DallasO <8305525+DallasO@users.noreply.github.com>
2019-04-05 11:12:35 -0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# [Checkout my Portfolio](https://dallaso.github.io/)
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.10191e902f1379eb828ed78b43ae958d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e8679a7ccba4e53b3615a260ac49623",
+    "url": "/static/media/iittala.5e8679a7.png"
+  },
+  {
+    "revision": "90d68de5cc43209dec924da44863f1f2",
+    "url": "/static/media/iittala-logo.90d68de5.svg"
+  },
+  {
+    "revision": "5cebe2f95485dc6b4e9645673f4dd3b9",
+    "url": "/static/media/crucible-brewing.5cebe2f9.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e908fe6233abe5b347f0",
+    "url": "/static/js/main.5ad07229.chunk.js"
+  },
+  {
+    "revision": "61433e735fbb2d010e13",
+    "url": "/static/js/2.b7f52e28.chunk.js"
+  },
+  {
+    "revision": "e908fe6233abe5b347f0",
+    "url": "/static/css/main.dda08edf.chunk.css"
+  },
+  {
+    "revision": "f0cbe271d0d2a6996d946c8cfae7293e",
+    "url": "/index.html"
+  }
+];
