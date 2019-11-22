3ca441f7b5f7232e117de011bf9ae4f77f3c25e6
DallasO <8305525+DallasO@users.noreply.github.com>
2019-11-21 22:00:28 -0800

    footer had improperly escaped html entities

--- src/components/generic/Footer.tsx
+++ src/components/generic/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer(): JSX.Element {
   function Copyright(): JSX.Element {
     const d = new Date().getFullYear();
     const copyright = d > 2019 ? `2019-${d}` : d;
-    return <div className="copyright">{`&copy;&nbsp;${copyright}`}</div>;
+    return <div className="copyright">&copy;&nbsp;{copyright}</div>;
   }
 
   return (
