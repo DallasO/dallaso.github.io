9b5555524a61f0500bd5a02eaa25d42366f7fda2
DallasO <8305525+DallasO@users.noreply.github.com>
2021-07-20 17:38:05 -0700

    feat: Add Valence

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,25 +8,10 @@ export default function AboutMe(): JSX.Element {
         <h2>Web Developer</h2>
       </div>
       <div className="wide">
-        <p>Full stack Web Developer located in the greater Seattle area.</p>
+        <p>Full stack Developer located in the greater Seattle area.</p>
         <p>
           {
-            "As a Web Developer, I've used many technologies to create beautiful frontends and featureful backends. I believe in mobile and UX first design."
-          }
-        </p>
-        <p>
-          {
-            "I'm currently helping a startup to revolutionize an industry while using React, Typescript, Node, Express, and MongoDB to name a few technologies. This statup is creating a marketing platform for producers of a niche product to communicate directly to their consumers, while giving consumers transparency into what they are buying."
-          }
-        </p>
-        <p>
-          {
-            "I've been a Web Developer for 5 years, working with directly with clients or business users. The projects I've worked on range from small hobby eCommerce stores, to large global companies. I've also worked with all kinds of languages and had to learn new technologies. See my list of skills below."
-          }
-        </p>
-        <p>
-          {
-            "One of my current projects is working with Python, Django, and React to analyze trends of a data set, and visualize the data with a charting framework. I'm looking forward to the challenges each step of the way."
+            "Building lightweight minimalist sites, large scalable solutions, immersive and interactive VR applications, and automation scripts. Using my range of skills to help sales estimate and bid on projects. Sharing my knowledge to propose solutions to interesting problems. Leading teams of developers to deliver projects successfully and on time. Communicating with and gathering requirements from clients."
           }
         </p>
       </div>
--- src/components/data/MyWorkData.tsx
+++ src/components/data/MyWorkData.tsx
@@ -8,18 +8,54 @@ import wamal from "../../img/wamal.png";
 
 export interface MyWorkProps {
   link: string;
-  title: string | JSX.Element;
-  description: string[];
-  image: string;
+  title: string;
+  titleAlt?: JSX.Element;
+  description: JSX.Element[];
+  image?: string;
   alt?: string;
 }
 
 export const MyWorkData: MyWorkProps[] = [
   {
+    link: "https://www.valencelevel.com/",
+    title: "Valence",
+    description: [
+      <>
+        Building potential by pushing boundaries and using the latest
+        technologies. Used Unity3D and targeted the Oculus Quest to build a
+        virtual training room experience for onboarding, knowledge archive, and
+        training. Created a reservation system and customer portal for launching
+        satelites into space via rideshare launches. Built websites using{" "}
+        <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+          ReactJS
+        </a>
+        ,{" "}
+        <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer">
+          GatsbyJS
+        </a>
+        ,{" "}
+        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+          NextJS
+        </a>{" "}
+        and{" "}
+        <a href="https://webflow.com/" target="_blank" rel="noreferrer">
+          Webflow
+        </a>
+        .
+      </>
+    ]
+    // image: cannabinder
+  },
+  {
     link: "https://www.Cannabinder.com",
     title: "Cannabinder",
     description: [
-      "Currently helping a startup bring a new marketing platform to market. This project is a public API powered by Node, Express, and MongoDB, as well as a frontend webapp powered by React and Typescript."
+      <>
+        Contributed to a startup that empowered consumers by giving them a
+        resource to find reliable and repeatable experiences. This project is a
+        public API powered by Node, Express, and MongoDB, as well as a frontend
+        webapp powered by React and Typescript.
+      </>
     ],
     image: cannabinder
   },
@@ -27,18 +63,28 @@ export const MyWorkData: MyWorkProps[] = [
     link: "https://www.fluke.com/en-us",
     title: "Fluke",
     description: [
-      "Worked directly with the marketing department for a Fortune 500 company, to create landing pages, emails, and online content for a global website. Managed content and translations to create responsive modern HTML. Used JavaScript, jQuery, and API's for marketing automation, tracking, and advanced user interactions."
+      <>
+        Worked directly with the marketing department for a Fortune 500 company,
+        to create landing pages, emails, and online content for a global
+        website. Managed content and translations to create responsive modern
+        HTML. Used JavaScript, jQuery, and API's for marketing automation,
+        tracking, and advanced user interactions.
+      </>
     ],
     image: fluke
   },
   {
     link: "https://iittala.com.au",
-    title: (
+    title: "Iittala",
+    titleAlt: (
       <img src={IittalaLogo} alt="Iittala" className="img-responsive logo" />
     ),
     description: [
-      "Completely rethemed the frontend as specified by a designer.",
-      "Cleaned up unused CSS from older themes. Integrated with existing XCart API for easy future updates."
+      <>Completely rethemed the frontend as specified by a designer.</>,
+      <>
+        Cleaned up unused CSS from older themes. Integrated with existing XCart
+        API for easy future updates.
+      </>
     ],
     image: iittala,
     alt: "Iittala"
@@ -47,8 +93,14 @@ export const MyWorkData: MyWorkProps[] = [
     link: "https://cruciblebrewing.com",
     title: "Crucible Brewing",
     description: [
-      "Helped a local business increase web and foot traffic by upgrading their website, while making it easier to maintain.",
-      "Installed WordPress, customized a theme, and added JS API call to load a menu that's always up to date."
+      <>
+        Helped a local business increase web and foot traffic by upgrading their
+        website, while making it easier to maintain.
+      </>,
+      <>
+        Installed WordPress, customized a theme, and added JS API call to load a
+        menu that&apos;s always up to date.
+      </>
     ],
     image: crucibleBrewing
   },
@@ -56,8 +108,16 @@ export const MyWorkData: MyWorkProps[] = [
     link: "https://wamal.rescuegroups.org",
     title: "WAMAL Malamute Rescue",
     description: [
-      "Volunteered my time to a local rescue group to help set up a new website. The site is hosted on a very limited platform, so there isn't a lot to customize. I mostly helped with theming, and setting guidelines for how content should be written.",
-      "I also installed affiliate and social links for donations and spreading the word."
+      <>
+        Volunteered my time to a local rescue group to help set up a new
+        website. The site is hosted on a very limited platform, so there isn't a
+        lot to customize. I mostly helped with theming, and setting guidelines
+        for how content should be written.
+      </>,
+      <>
+        I also installed affiliate and social links for donations and spreading
+        the word.
+      </>
     ],
     image: wamal,
     alt: "Washington Alaskan Malamute Adoption League (WAMAL)"
